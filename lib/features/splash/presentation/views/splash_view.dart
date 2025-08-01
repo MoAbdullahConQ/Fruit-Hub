f3763080cd7af0b1d8f95ab44113412ef0ce0760
Mu Abdullah <mohamed553231@gmail.com>
2025-08-01 23:58:13 +0300

    Splash view ui done

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,12 +1,13 @@
+import 'package:ecommerce_app/features/splash/presentation/views/widgets/splash_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
-  static const routeName ='splash';
+  static const routeName = 'splash';
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: SplashViewBody());
   }
 }
---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,26 @@
+import 'package:ecommerce_app/core/utils/app_images.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: [
+            SvgPicture.asset(Assets.imagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(Assets.imagesSplashBottom,
+        fit: BoxFit.fill,
+        ),
+      ],
+    );
+  }
+}
