6a0e99082c8400be3fa76ce4526e86a2605f5493
Mu Abdullah <mohamed553231@gmail.com>
2025-08-23 15:30:53 +0300

    Login ui part two create appbar

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,12 +1,17 @@
+import 'package:ecommerce_app/features/auth/presentation/views/widgets/custom_app_bar.dart';
+import 'package:ecommerce_app/features/auth/presentation/views/widgets/login_view_body.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
-static const String routeName = 'login';
+  static const String routeName = 'login';
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: buildAppBar(context, title: 'تسجيل الدخول'),
+      body: SafeArea(child: LoginViewBody()),
+    );
   }
 }
---
+++ lib/features/auth/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerce_app/core/utils/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+AppBar buildAppBar(context, {required String title}) {
+  return AppBar(
+    leading: GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Icon(Icons.arrow_back_ios_new),
+    ),
+
+    centerTitle: true,
+    title: Text(title, style: TextStyles.bold19),
+  );
+}
