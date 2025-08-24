fa96fedec5af69c4ed2368246d1aeb5e73e866a4
Mu Abdullah <mohamed553231@gmail.com>
2025-08-24 11:34:54 +0300

    Signup ui part 2

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_app/constants.dart';
+import 'package:ecommerce_app/features/auth/presentation/views/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 
 class SignupViewBody extends StatelessWidget {
@@ -5,6 +7,29 @@ class SignupViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizintalPadding),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 24),
+              CustomTextFormField(hintText: 'الاسم كامل', keyboardType: TextInputType.name),
+              SizedBox(height: 16),
+              CustomTextFormField(hintText: 'البريد الإلكتروني', keyboardType: TextInputType.emailAddress),
+              SizedBox(height: 16), 
+              CustomTextFormField(
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.remove_red_eye, color: Color(0xFFC9CECF)),
+                ),
+                hintText: 'كلمة المرور',
+                keyboardType: TextInputType.visiblePassword,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
