3374b8440a13276d4bc98c7dbd9f6462ca89d5be
Mu Abdullah <mohamed553231@gmail.com>
2025-08-24 18:12:45 +0300

    Implement auth repo create user with email and password

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,20 @@
+import 'package:ecommerce_app/features/auth/domain/entites/user_entity.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserModel extends UserEntity {
+  UserModel({
+    required super.email,
+    required super.name,
+    required super.uId,
+  });
+
+  factory UserModel.fromFirebaseUser(User user) {
+    return UserModel(
+      email: user.email ?? '',
+      name: user.displayName ?? '',
+      uId: user.uid,
+    );
+  }
+
+
+}
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,16 +1,32 @@
 import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/core/errors/custom_exeptions.dart';
 import 'package:ecommerce_app/core/errors/failures.dart';
+import 'package:ecommerce_app/core/services/firebase_auth_service.dart';
+import 'package:ecommerce_app/features/auth/data/models/user_model.dart';
 import 'package:ecommerce_app/features/auth/domain/entites/user_entity.dart';
 import 'package:ecommerce_app/features/auth/domain/repos/auth_repo.dart';
 
 class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthService firebaseAuthService;
+
+  AuthRepoImpl({required this.firebaseAuthService});
+
   @override
   Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
     String email,
     String password,
-  ) {
-
-    
-    throw UnimplementedError();
+    String name,
+  ) async {
+    try {
+      var user = await firebaseAuthService.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return Right(UserModel.fromFirebaseUser(user));
+    } on CustomExeptions catch (e) {
+      return Left(ServerFailure(e.message));
+    } catch (e) {
+      return Left(ServerFailure('An error occurred.Please try again.'));
+    }
   }
 }
