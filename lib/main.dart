d485b821ef5ee4dbfa89344b154021a6905f4c37
Mu Abdullah <mohamed553231@gmail.com>
2025-08-24 17:34:20 +0300

    Create firebase auth service add Firebase initialization and authentication repository implementation

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,12 @@ import 'package:ecommerce_app/features/splash/presentation/views/splash_view.dar
 import 'package:ecommerce_app/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs().init();
   runApp(const MyApp());
