8976f3d874b36a85354cdac6409983883fa81ae1
Mu Abdullah <mohamed553231@gmail.com>
2026-04-30 19:15:27 +0300

    Create Abstract Database Service

---
+++ lib/core/services/firestore_service.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerce_app/core/services/database_service.dart';
+
+class FirestoreService implements DatabaseService{
+  @override
+  Future<void> addData({required String path, required Map<String, dynamic> data}) {
+    
+  }
+}
+
