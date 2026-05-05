90855d40aea0dca5b812335a4777d4c61586727c
Mu Abdullah <mohamed553231@gmail.com>
2026-05-05 12:08:57 +0300

    feat(home): Best selling header Add Best Selling Header to home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/core/widgets/search_text_field.dart';
+import 'package:ecommerce_app/features/home/presentation/views/widgets/best_selling_header.dart';
 import 'package:ecommerce_app/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:ecommerce_app/features/home/presentation/views/widgets/featured_list.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,9 @@ class HomeViewBody extends StatelessWidget {
                 SizedBox(height: 16),
                 SearchTextField(),
                 SizedBox(height: 12),
-                FeaturedList()
+                FeaturedList(),
+                SizedBox(height: 12),
+                BestSellingHeader(),
               ],
             ),
           ),
